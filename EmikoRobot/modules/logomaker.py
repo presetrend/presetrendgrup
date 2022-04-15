1c7e24a71618b11099e36d62d7860ed16e49279c
presetrend <103375708+presetrend@users.noreply.github.com>
2022-04-15 09:40:37 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo dibuat oleh [fahri Robot](https://t.me/presetrendmanagebot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo dibuat oleh [fahri Robot](https://t.me/presetrendmanagebot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -210,12 +210,12 @@ async def logo_(event):
 
 __mod_name__ = "Logomaker"
 
-__help__ = """ This is help menu for logomaker
+__help__ = """ Ini adalah menu bantuan untuk pembuat logo
 
-❂ /logo <text/name> - Create a logo with random view.
-❂ /wlogo <text/name> - Create a logo with wide view only.
+❏ /logo <text/name> - Buat logo dengan tampilan acak.
+❏ /wlogo <text/name> - Buat logo dengan tampilan lebar saja.
 
- Image Editor :
+ Editor Gambar :
 
-❂  /edit <reply photo> - to edit image.
+❏  /edit <balas gambar> - untuk mengedit gambar
 """
